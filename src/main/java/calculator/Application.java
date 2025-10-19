03c2012f9e189e20935495f53d866665150f896e
nnam01 <isthenam@naver.com>
2025-10-19 18:04:07 +0900

    feat: 예외 처리 기능 추가

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -9,10 +9,14 @@ public class Application {
 
         System.out.println("덧셈할 문자열을 입력해 주세요.");
         String input = Console.readLine();
-
-        int result = addNumberInString(input);
-        System.out.println("결과 : " + result);
+        try {
+            int result = addNumberInString(input);
+            System.out.println("결과 : " + result);
+        } catch (Throwable e) {
+            throw new IllegalArgumentException("Error : " + e.getMessage());
+        }
     }
+
 
     public static int addNumberInString(String input) {
         if (input.startsWith("//")) {
@@ -20,7 +24,15 @@ public class Application {
             String customDelim = input.substring(2, newLineIndex);
             input = input.substring(newLineIndex + 2).replace(customDelim, ",");
         }
-        
+
+        if (input.trim().isEmpty()) {
+            return 0;
+        }
+
+        if (input.contains("-")) {
+            throw new Error("Not allowed negative number.");
+        }
+
         StringTokenizer tokenizer = new StringTokenizer(input, ",:", false);
         int sum = 0;
         while (tokenizer.hasMoreTokens()) {
